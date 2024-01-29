fc66bda85f8419c92727b49dc9e2e77704a8bca3
HyunjinKim <koujie11@gmail.com>
2024-01-29 20:48:23 +0900

    fix : todo sort 함수 실행 완료

--- src/component/todo/TodoOrder.jsx
+++ src/component/todo/TodoOrder.jsx
@@ -1,18 +1,22 @@
-import React from "react";
+import { useState } from "react";
 
-function TodoOrder({
-  order,
-  changeDateUpOrderHandler,
-  changeDateDownOrderHandler,
-}) {
+function TodoOrder({ sortCards }) {
+  // order 순으로 radio 버튼 누르면 오름차순 정렬
+  const changeDateOrderHandler = (e) => {
+    const nextSortOrder = e.target.value;
+    // console.log(nextSortOrder);
+
+    sortCards(nextSortOrder);
+  };
+
   return (
     <form className="order-radio">
       <label>
         <input
           type="radio"
           name="option"
-          value={order}
-          onChange={changeDateUpOrderHandler}
+          value="asc"
+          onChange={changeDateOrderHandler}
         />
         &nbsp; 마감 빠른순
       </label>
@@ -20,8 +24,8 @@ function TodoOrder({
         <input
           type="radio"
           name="option"
-          value={order}
-          onChange={changeDateDownOrderHandler}
+          value="desc"
+          onChange={changeDateOrderHandler}
         />
         &nbsp; 마감 느린순
       </label>
