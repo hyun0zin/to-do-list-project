7fa8a684ee04078ae3bd782e915869999381f045
HyunjinKim <koujie11@gmail.com>
2024-02-03 00:18:42 +0900

    fix : GlobalStyle 적용

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,24 @@ import React from "react";
 import "./App.css";
 import TodoHeader from "component/layout/Header";
 import TodoController from "component/todo/TodoController";
+import styled from "styled-components";
+import GlobalStyle from "GlobalStyle";
+
+const StDiv = styled.div`
+  padding: 20px;
+
+  min-width: 800px;
+  max-width: 1200px;
+`;
 
 const App = () => {
   return (
     <>
-      <div className="app-style">
+      <GlobalStyle />
+      <StDiv className="app-style">
         <TodoHeader />
         <TodoController />
-      </div>
+      </StDiv>
     </>
   );
 };
---
+++ src/GlobalStyle.jsx
@@ -0,0 +1,9 @@
+const { createGlobalStyle } = require("styled-components");
+
+const GlobalStyle = createGlobalStyle`
+display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+export default GlobalStyle;
