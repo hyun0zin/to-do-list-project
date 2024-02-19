5aff424e820c20ddbbe0c1cf7f5b30c75579f91c
HyunjinKim <koujie11@gmail.com>
2024-02-19 09:46:25 +0900

    style : css 수정

--- src/pages/DetailPage.jsx
+++ src/pages/DetailPage.jsx
@@ -1,10 +1,12 @@
 import TodoCards from "component/todo/TodoCards";
 import { CardContext } from "context/CardContext";
 import { useContext } from "react";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
+import styled from "styled-components";
 
 function DetailPage() {
   const params = useParams();
+  const navigate = useNavigate();
   const { cards } = useContext(CardContext);
   console.log(cards[0].id);
   console.log(params.id);
@@ -12,12 +14,31 @@ function DetailPage() {
 
   const todo = cards.find((todo) => todo.id === params.id);
   // console.log(todo);
-
+  const backToHomeClickHandler = () => {
+    navigate("/");
+  };
   return (
     <>
-      <TodoCards todo={todo} />
+      <BackToHomeBtn onClick={backToHomeClickHandler}>홈으로</BackToHomeBtn>
+      <CardContainer>
+        <TodoCards todo={todo} />
+      </CardContainer>
     </>
   );
 }
 
 export default DetailPage;
+
+const CardContainer = styled.div`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+`;
+
+const BackToHomeBtn = styled.button`
+  border-color: transparent;
+  background-color: transparent;
+  padding: 1rem;
+  font-weight: 600;
+`;
